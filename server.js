1cdff07f2e82467f6e24fc94ce5b403bae6c3371
David <grayd500@gmail.com>
2023-11-07 15:04:25 -0500

    Update home and main handlebars for cleaner navigation

--- server.js
+++ server.js
@@ -30,26 +30,20 @@ app.use((req, res, next) => {
 });
 
 const hbs = exphbs.create({
-  /* config */
 });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
 
-// Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-// Use routes
 app.use('/dashboard', dashboardRoutes);
 app.use(routes);
 
-// Sync Sequelize models to the database, then start the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
