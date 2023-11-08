d6f8b7b25655c1c8227a0a4a282fce0a1f3a2f94
David <grayd500@gmail.com>
2023-11-08 01:37:36 -0500

    Fixed event listener issue with logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,28 +1,32 @@
 // public/js/logout.js
 document.addEventListener('DOMContentLoaded', () => {
-  const logoutButton = document.getElementById('logoutButton');
+    const logoutButton = document.getElementById('logoutButton');
   
-  logoutButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      
-      fetch('/api/users/logout', {
+    // Check if the logoutButton element exists
+    if (logoutButton) {
+      logoutButton.addEventListener('click', (event) => {
+        event.preventDefault();
+  
+        fetch('/api/users/logout', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' }
-      })
-      .then(response => {
-          if (response.ok) {
+        })
+          .then(response => {
+            if (response.ok) {
               window.location.href = '/login';
-          } else {
+            } else {
               // Instead of throwing an error, handle the failed response
               response.text().then(text => {
-                  console.error('Logout failed:', text);
-                  alert(text);
+                console.error('Logout failed:', text);
+                alert(text);
               });
-          }
-      })
-      .catch((error) => {
-          console.error('Logout error:', error);
-          alert('An error occurred while trying to log out.');
+            }
+          })
+          .catch((error) => {
+            console.error('Logout error:', error);
+            alert('An error occurred while trying to log out.');
+          });
       });
+    }
   });
-});
+  
