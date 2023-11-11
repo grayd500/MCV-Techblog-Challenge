b5ed0f59f3e61924b8ec16ec40094eed7e33536f
David <grayd500@gmail.com>
2023-11-10 22:50:03 -0500

    Configure database for Heroku deployment

--- config/config.js
+++ config/config.js
@@ -2,11 +2,27 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: 'localhost',
-  dialect: 'mysql',
-  // ... rest of your config
-});
+// Check for JAWSDB_URL to use on Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // This line is necessary for Heroku deployment
+      }
+    }
+  });
+} else {
+  // Fallback to local .env variables if JAWSDB_URL isn't set
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: 'localhost',
+    dialect: 'mysql',
+    // ... rest of your local config
+  });
+}
 
 module.exports = sequelize;
+
